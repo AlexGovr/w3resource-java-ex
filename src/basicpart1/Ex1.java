95b3461fc0a926e9b64b349c804993f220fe95b4
alex <sobachka.94@mail.ru>
2021-04-06 05:10:21 -0400

    complete Ex1

--- src/basicpart1/Ex1.java
+++ src/basicpart1/Ex1.java
@@ -1,7 +1,13 @@
 package basicpart1;
 
+import java.util.Scanner;
+
 public class Ex1 {
     public static void main(String args[]) {
         System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What's your name?");
+        String name = new String(scan.nextLine());
+        System.out.println("Hello, " + name + "!");
     }
 }
