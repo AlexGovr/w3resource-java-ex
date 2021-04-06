ea0e5a3351167eeb99d8c273032729a49a60ce91
alex <sobachka.94@mail.ru>
2021-04-06 05:28:09 -0400

    complete Ex4

---
+++ src/basicpart1/Ex4.java
@@ -0,0 +1,11 @@
+package basicpart1;
+
+public class Ex4 {
+
+    public static void main(String args[]) {
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55+9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3 );
+    }
+}
