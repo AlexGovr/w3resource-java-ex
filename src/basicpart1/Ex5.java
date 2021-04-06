31f52f9bd0bc30fff168b5a26bf67a0cc05f8942
alex <sobachka.94@mail.ru>
2021-04-06 05:32:09 -0400

    complete Ex5

---
+++ src/basicpart1/Ex5.java
@@ -0,0 +1,17 @@
+package basicpart1;
+
+import java.util.Scanner;
+
+public class Ex5 {
+    
+    public static void main(String args[]) {
+        double a, b;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("First number, please:");
+        a = scan.nextDouble();
+        System.out.println("Second number, please:");
+        b = scan.nextDouble();
+        System.out.println(a * b);
+    }
+}
