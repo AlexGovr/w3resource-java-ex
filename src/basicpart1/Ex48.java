bcd353c123db3b6e1d18cf53b8708610c4812fc8
alex <sobachka.94@mail.ru>
2021-04-07 12:12:03 -0400

    update task

--- src/basicpart1/Ex48.java
+++ src/basicpart1/Ex48.java
@@ -1,17 +1,15 @@
 package basicpart1;
 
-import java.util.Scanner;
-
 public class Ex48 {
     
     public static void main(String args[]) {
         for (int n = 0; n < 100; n++){
-            if (is_odd(n))
+            if (n % 2 != 0)
                 System.out.println(n);
         }
     }
 
-    public static boolean is_odd(int d) {
+    public static boolean is_prime(int d) {
         for (int i = 2; i < (int) Math.sqrt(d) + 1; i++) {
             if ((d % i) == 0)
                 return false;
